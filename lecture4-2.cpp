86622cd62d1525f3be70985679e921c5a2ae3f51
vrinda vinesh <vrinda1103@gmail.com>
2023-07-19 17:17:25 +0530

    version 4-2

---
+++ lecture4-2.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std; 
+int main() {
+    
+    
+int a;
+
+cout<<"enter an integer:";
+
+cin>>a;
+
+switch(a){
+    case 1:
+    cout<<"the value of a is 1";
+    break;
+    
+    case 2:
+    cout<<"the value of a is 2";
+    break;
+    
+    default:
+    cout<<"default will be always printed";
+    break;
+}
+    return 0;
+}
